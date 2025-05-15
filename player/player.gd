e3b6d72ea432f0695b73ccbeb578c82cea3db620
Yarilloslav <kurepin.06@mail.ru>
2025-05-16 00:25:50 +0300

    really fixed)

--- player/player.gd
+++ player/player.gd
@@ -10,9 +10,19 @@ const JUMP_INNERT: int = 8
 var JUMP_COUNT: int = 0
 const MAX_JUMPS: int = 2
 const SPRINT_SPEED: float = 400.0
-
+var health: int = 10
 
 var gravity: float = 980
 @onready var animation = $AnimatedSprite2D
 
+
+func take_damage(amount: int) -> void:
+	health -= amount
+	print("Игрок получил урон! Осталось HP: ", health)
+	if health <= 0:
+		die()
+
+func die() -> void:
+	print("Игрок погиб!")
+	queue_free()
 signal u_turn
